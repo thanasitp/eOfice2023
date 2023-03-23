df97db061f0071d669b10c9355d98da64f1de6e1
thanasitp <thanasitnavy60@gmail.com>
2023-03-23 11:15:45 +0700

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  userData: any;
+  actionNumber: number = 0;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
+
+  openNav() {
+    //ความกว้างของ slide menu
+    //document.getElementById("mySidenav").style.width = "300px";
+  }
+
+  closeNav() {
+    //document.getElementById("mySidenav").style.width = "0";
+  }
 }
